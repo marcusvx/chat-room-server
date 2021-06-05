00627da5eae880b3a2c85e30a5b39173687090f7
Marcus V. Ximenes <marcus@marcusvinicius.info>
2021-06-05 20:52:37 -0300

    Adds scripts to create all database tables

--- sql/data.sql
+++ sql/data.sql
@@ -1,11 +1,178 @@
-INSERT INTO ChatRooms.Category(Name)
-VALUES ('Friendship');
+USE chat_rooms;
 
-INSERT INTO ChatRooms.Category(Name)
-VALUES ('Music');
+/*
+    ROOMS
+*/
+INSERT INTO room(`id`, `name`)
+VALUES (1, 'General');
 
-INSERT INTO ChatRooms.Category(Name)
-VALUES ('Movies');
+INSERT INTO room(`id`, `name`)
+VALUES (2, 'Memes');
 
-INSERT INTO ChatRooms.Category(Name)
-VALUES ('Books');
+INSERT INTO room(`id`, `name`)
+VALUES (3, 'Dad Jokes');
+
+INSERT INTO room(`id`, `name`)
+VALUES (4, 'Software Development');
+
+INSERT INTO room(`id`, `name`)
+VALUES (5, 'Music');
+
+INSERT INTO room(`id`, `name`)
+VALUES (6, 'Movies');
+
+INSERT INTO room(`id`, `name`)
+VALUES (7, 'Books');
+
+INSERT INTO room(`id`, `name`)
+VALUES (8, 'Cuisine');
+
+INSERT INTO room(`id`, `name`)
+VALUES (9, 'Politics');
+
+/*
+    USERS
+*/
+INSERT INTO user(`id`, `name`)
+VALUES (1, 'Bob');
+
+INSERT INTO user(`id`, `name`)
+VALUES (2, 'Kate');
+
+INSERT INTO user(`id`, `name`)
+VALUES (3, 'Jack');
+
+INSERT INTO user(`id`, `name`)
+VALUES (4, 'Maria');
+
+INSERT INTO user(`id`, `name`)
+VALUES (5, 'John');
+
+INSERT INTO user(`id`, `name`)
+VALUES (6, 'Jane');
+
+/*
+    EVENTS
+*/
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:23:48', 1, 'Enter', null);
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:24:10', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Hey, someone there???' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:34:00', 2, 'Enter', null);
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:36:10', 2, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Whats up?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:37:00', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Hey Kate, how`s going?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:37:30', 2, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Good! You?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:38:43', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'superb! Did you hear the news?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:39:02', 2, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Nope. What news? Good ones?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:41:20', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Yeah, great news. Sarah is coming to town next weekend. We`re planning a welcome party at Jonh`s place on Staurday night' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:42:20', 2, 'HighFive', 1); 
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:43:02', 2, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'That`s great! Can I help with something? For the party I mean...' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:44:00', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Just be there and bring some beers ' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 07:47:01', 2, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'OK' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:20:10', 3, 'Enter', null);
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:21:00', 3, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Hi guys' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:22:20', 2, 'HighFive', 3); 
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:26:00', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Hey Jack, I just told Kate about Sarah`s party. You`re coming too, right?' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:32:00', 3, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'I don`t know yet, I have that job to finish. Maybe I`ll show up a little late' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:33:00', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'OK, I hope to see you there' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:41:00', 4, 'Enter', null); 
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:42:00', 4, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'Hey everyone' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:43:20', 2, 'HighFive', 4); 
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:44:20', 3, 'HighFive', 4); 
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:47:00', 1, 'Comment', null); 
+
+INSERT INTO event_comment(`event_id`, `content`)
+SELECT MAX(`id`), 'I need to go. Catch up with you later. Bye!' FROM `event`;
+
+INSERT INTO event(`room_id`, `received_at`, `from_user_id`, `event_type`, `to_user_id`)
+VALUES (1, '2020-08-22 08:48:00', 1, 'Leave', null); 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,35 @@
-CREATE TABLE ChatRooms.Category (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL
+USE chat_rooms;
+
+CREATE TABLE `room` (
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `user` (
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `event` (
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    `room_id` INT UNSIGNED NOT NULL,
+    `received_at` TIMESTAMP NOT NULL,
+    `from_user_id` INT UNSIGNED NOT NULL,
+    `event_type` ENUM ('Enter', 'Leave', 'Comment', 'HighFive') NOT NULL,
+    `to_user_id` INT UNSIGNED,
+    PRIMARY KEY(`id`),
+    FOREIGN KEY (`room_id`) REFERENCES `room`(`id`),
+    FOREIGN KEY (`from_user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY (`to_user_id`) REFERENCES `user`(`id`)
+);
+
+CREATE INDEX index_event_received_at ON `event`(received_at);
+
+CREATE TABLE `event_comment` (
+    `event_id` INT UNSIGNED NOT NULL,
+    `content` TINYTEXT NOT NULL,
+    PRIMARY KEY (`event_id`),
+    FOREIGN KEY (`event_id`) REFERENCES `event`(`id`)
 );
